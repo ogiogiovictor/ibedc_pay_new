6c5cb2707142b84eda8cf08104bc7d3588023c51
Victor Ogiogio <victor.ogiogio@ibedc.com>
2024-02-21 22:06:46 +0100

    continue asking

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -275,263 +275,12 @@
                       </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 col-xl-12 grid-margin stretch-card">
-                      <div class="card">
-                        <div class="card-body">
-                          <div class="d-flex flex-wrap justify-content-between">
-                            <div>
-                              <h4 class="card-title mb-3">Revenue overview</h4>
-                            </div>
-                            <div>
-                              <div class="d-flex align-items-center">
-                                <div class="dropdown mr-2 mb-2 d-none d-md-block">
-                                  <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuSizeButton4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                  2019
-                                  </button>
-                                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuSizeButton4" data-x-placement="bottom-end">
-                                    <a class="dropdown-item" href="#">2015</a>
-                                    <a class="dropdown-item" href="#">2016</a>
-                                    <a class="dropdown-item" href="#">2017</a>
-                                    <a class="dropdown-item" href="#">2018</a>
-                                  </div>
-                                </div>
-                                <div class="dropdown dropleft card-menu-dropdown">
-                                  <button class="btn p-0" type="button" id="dropdown5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical card-menu-btn"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdown5" x-placement="left-start">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <p class="text-muted">Customers who have upgraded the level of your products or service</p>
-                          <div class="mt-4 mb-4 d-sm-flex">
-                            <div id="legendContainer" class="mb-4 mr-4 legendContainer col-md-4 pl-0 pr-0"></div>
-                            <div class="col-md-6 pl-0 pr-0">
-                              <h6>Summary</h6>
-                              <p class="text-muted">A comparison of people who mark themeselves of their interest based from the date range given above.</p>
-                            </div>
-                          </div>
-                          <div class="row mt-1 d-sm-flex">
-                            <div class="col-12">
-                                <canvas id="revenue-overview"></canvas>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-lg-4 d-flex flex-column">
-                      <div class="row flex-grow">
-                        <div class="col-12 col-lg-4 col-lg-12 grid-margin stretch-card">
-                          <div class="card">
-                            <div class="card-body">
-                              <div class="d-flex flex-wrap justify-content-between">
-                                <h4 class="card-title mb-3">Sales top charts</h4>
-                                <div class="dropdown dropleft card-menu-dropdown">
-                                  <button class="btn p-0" type="button" id="dropdown6" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical card-menu-btn"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdown6" x-placement="left-start">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="row">
-                                <div class="col-12">
-                                  <div class="row">
-                                    <div class="col-12">
-                                      <p class="text-muted mb-4 text-left">Customers who have upgraded the level of your products or service</p>
-                                    </div>
-                                  </div>
-                                  <div class="row">
-                                    <div class="col-12">
-                                      <canvas id="salesTopChart"></canvas>
-                                    </div>
-                                  </div>
-                                  <div class="row mt-5 d-none d-sm-flex">
-                                    <div class="col-12 col-sm-6 text-left">
-                                      <h1>891</h1>
-                                      <p class="font-weight-normal text-muted">North Ludwig</p>
-                                    </div>
-                                    <div class="col-12 col-sm-6 text-left">
-                                      <h1>227</h1>
-                                      <p class="font-weight-normal text-muted">North Ludwig</p>
-                                    </div>
-                                  </div>
-                                  <div class="row pt-3 mt-md-4">
-                                    <div class="col mb-2">
-                                      <div class="d-flex sales-top-chart-legend align-items-center">
-                                        <div class="bg-info p-3 mr-3 mr-lg-0 mr-lg-3 mb-md-2 mb-lg-0">
-                                          <canvas id="acquisition-bar_1" height="20" width="20"></canvas>
-                                        </div>
-                                        <div class="wrapper d-flex flex-column justify-content-center">
-                                          <p class="font-weight-medium text-muted">Budget</p>
-                                          <h3 class="font-weight-medium mb-0">$12,783</h3>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div class="col">
-                                      <div class="d-flex sales-top-chart-legend align-items-center">
-                                        <div class="bg-success p-3 mr-3 mr-lg-0 mr-lg-3 mb-md-2 mb-lg-0">
-                                          <canvas id="acquisition-bar_2" height="20" width="20"></canvas>
-                                        </div>
-                                        <div class="wrapper d-flex flex-column justify-content-center">
-                                          <p class="font-weight-medium text-muted">Sales</p>
-                                          <h3 class="font-weight-medium mb-0">9,065</h3>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div class="row">
-                                    <div class="col-12 mt-4">
-                                      <p class="text-muted text-left">Lorem Ipsum is simply dummy text of the printing</p>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-4 d-flex flex-column">
-                      <div class="row flex-grow">
-                        <div class="col-md-6 col-lg-12 grid-margin stretch-card">
-                          <div class="card bg-primary">
-                            <div class="card-body pb-0">
-                              <div class="d-flex flex-wrap justify-content-between">
-                                <h4 class="card-title text-white">Marketing</h4>
-                                <div class="dropdown dropleft card-menu-dropdown">
-                                  <button class="btn p-0" type="button" id="dropdown7" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical card-menu-btn"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdown7" x-placement="left-start">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="d-flex flex-wrap align-items-center justify-content-between">
-                                <div>
-                                  <h6 class="text-white">Past year</h6>
-                                  <h1 class="text-white">46360</h1>
-                                </div>
-                                <button class="btn btn-outline-primary btn-fw border ml-xl-4 text-white mb-4">View more</button>
-                              </div>
-                            </div>
-                            <div class="card-body p-0">
-                              <div>
-                                <canvas id="areaChartMarketing"></canvas>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 grid-margin stretch-card">
-                          <div class="card">
-                            <div class="card-body">
-                              <div class="d-flex flex-wrap align-items-baseline justify-content-between">
-                                <h4 class="card-title">Activity</h4>
-                                <div class="dropdown dropleft card-menu-dropdown">
-                                  <button class="btn p-0" type="button" id="dropdown8" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical card-menu-btn"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdown8" x-placement="left-start">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                                </div>
-                              </div>
-                              <div>
-                                <ul class="solid-icon-list">
-                                  <li>
-                                    <h5>Offline purchases</h5>
-                                    <h6 class="text-muted font-weight-normal">A comparison of people who mark
-                                      <span class="mt-3">30 Min ago</span>
-                                    </h6>
-                                  </li>
-                                  <li>
-                                    <h5>Offline purchases</h5>
-                                    <h6 class="text-muted font-weight-normal">A comparison of people who mark
-                                      <span class="mt-3">30 Min ago</span>
-                                    </h6>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-4 d-flex flex-column">
-                      <div class="row flex-grow">
-                        <div class="col-12 col-md-12 col-lg-12 grid-margin stretch-card">
-                          <div class="card">
-                            <div class="card-body">
-                              <div class="d-flex flex-wrap justify-content-between">
-                                <h4 class="card-title mb-3">Purchases in detail</h4>
-                                <div class="dropdown dropleft card-menu-dropdown">
-                                  <button class="btn p-0" type="button" id="dropdown9" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical card-menu-btn"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdown9" x-placement="left-start">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="row">
-                                <div class="col-12">
-                                  <div class="chartjs-legend mt-4" id="chart-legends-purchase"></div>
-                                  <div class="row">
-                                    <div class="col-12">
-                                      <canvas id="purchaseDetails"></canvas>
-                                    </div>
-                                  </div>
-                                  <div class="row pt-3 mt-md-4">
-                                    <div class="col">
-                                      <div class="d-flex purchase-detail-legend align-items-center">
-                                          <div id="circleProgress1" class="p-2"></div>
-                                          <div>
-                                            <p class="font-weight-medium text-muted text-small">Sessions</p>
-                                            <h3 class="font-weight-medium  mb-0">26.80%</h3>
-                                          </div>
-                                      </div>
-                                    </div>
-                                    <div class="col">
-                                      <div class="d-flex purchase-detail-legend align-items-center">
-                                          <div id="circleProgress2" class="p-2"  width="42" height="42"></div>
-                                          <div>
-                                            <p class="font-weight-medium text-muted text-small">Users</p>
-                                            <h3 class="font-weight-medium  mb-0">56.80%</h3>
-                                          </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div class="row">
-                                    <div class="col-12 mt-4">
-                                      <p class="text-muted text-left">Lorem Ipsum is simply dummy text of the printing</p>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+
+
+              
+
+
+
                   <div class="row">
                     <div class="col-12 col-lg-4 col-xl-4 grid-margin stretch-card">
                       <div class="card">
@@ -616,9 +365,7 @@
                                 <i class="mdi mdi-dots-vertical card-menu-btn"></i>
                               </button>
                               <div class="dropdown-menu" aria-labelledby="dropdown11" x-placement="left-start">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
+                                <a class="dropdown-item" href="#">Successful</a>
                               </div>
                             </div>
                           </div>
@@ -626,22 +373,22 @@
                           <div class="border pt-2 pb-2 mt-4 mb-3 border-radius-widget">
                             <ul class="d-md-flex flex-wrap align-items-baseline justify-content-center list-unstyled text-center mb-0 sales-legend">
                               <li class="border-right-sm">
-                                <h6 class="font-weight-normal">Total</h6>
+                                <h6 class="font-weight-normal">Today's Sale</h6>
                                 <h2 class="text-primary">2584</h2>
                                 <p class="text-primary pl-md-4 pr-md-4">56.04 % Total</p>
                               </li>
                               <li class="border-right-sm">
-                                <h6 class="font-weight-normal">This Year</h6>
+                                <h6 class="font-weight-normal">This Months </h6>
                                 <h2 class="text-primary pl-md-3 pr-3">46360</h2>
                                 <p class="text-primary pl-3 pr-3">32.68 % Total</p>
                               </li>
                               <li class="border-right-sm">
-                                <h6 class="font-weight-normal">Past year</h6>
+                                <h6 class="font-weight-normal">This Year</h6>
                                 <h2 class="text-primary">46360</h2>
                                 <p class="text-primary">97.32% Total</p>
                               </li>
                               <li class="pb-2 pt-2 pl-4 pr-4">
-                                <h6 class="font-weight-normal">Difference</h6>
+                                <h6 class="font-weight-normal">All Sales</h6>
                                 <h2 class="text-primary">93819</h2>
                                 <p class="text-primary">76.47% Total</p>
                               </li>
