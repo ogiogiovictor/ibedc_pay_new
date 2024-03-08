2c888187996183ed04f0bd5873dd5116266dcc9b
Victor Ogiogio <victor.ogiogio@ibedc.com>
2024-03-08 22:03:53 +0100

    Superman is dead

--- resources/views/livewire/log-details.blade.php
+++ resources/views/livewire/log-details.blade.php
@@ -29,7 +29,68 @@
                           
                           </div>
                           <div class="table-responsive">
-                            content goes here
+                            
+                          <ul class="bullet-line-list">
+										<li>
+											<h6>Date Logged</h6>
+											<p>{{ $details->created_at }} </p>
+											<p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>User ID</h6>
+											<p>{{ $details->user_id }} </p>
+											<p class="text-muted mb-4">
+												<i class="mdi mdi-clock-outline"></i>
+											</p>
+										</li>
+
+                                        <li>
+											<h6>IP Address</h6>
+											<p>{{ $details->ip_address }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>URL </h6>
+											<p>{{ $details->url }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>Method </h6>
+											<p>{{ $details->method }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>User Agent </h6>
+											<p>{{ $details->user_agent }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>Payload </h6>
+											<p>{{ $details->payload }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>Status Code </h6>
+											<p class="btn btn-warning">{{ $details->status_code }} </p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+
+                                        <li>
+											<h6>Response Code </h6>
+                                            <p>{!! $details->response !!}</p>
+                                            <p class="text-muted mb-4"></p>
+										</li>
+										
+									</ul>
+
+                                 
+
                           </div>
                         </div>
                       </div>
