a85b5eb44aa9990b6fe875aa12d92f3b4f4d11a9
Ogiogio Victor <ogiogiovictor@gmail.com>
2024-07-26 07:35:53 +0000

    running mouth

---
+++ app/Services/PolarisLogService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Controllers\BaseAPIController;
+use App\Models\Polaris\PolarisPaymentPayload;
+
+
+class PolarisLogService extends BaseAPIController
+{
+    
+    public function processLogs($transactionid, $meterno = null, $accountno = null, $payload)
+    {
+        try {
+            $createAccount = PolarisPaymentPayload::create([
+                'transaction_id' => $transactionid,
+                'meter_no' => $meterno,
+                'account_no' => $accountno,
+                'payload' => $payload
+            ]);
+
+            return response()->json(['message' => 'Log created successfully'], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
